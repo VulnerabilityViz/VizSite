10d7ec4d6ab9fe2079bcde3ce7a581d347854589
Toby Lin <zhixiang94@gmail.com>
2015-12-09 14:01:24 -0500

    Added data model

--- models/data.js
+++ models/data.js
@@ -1,5 +1,8 @@
 'use strict'
-
+/*
+	 source, version, upstream_version, branch, urgency, date, 
+	 previous_source, previous_version, cve_list, maintainer, comments
+*/
 require('mongoose-schema-extend');
 var BaseModel = require('./lib/model'),
     mongoose = require('mongoose'),
@@ -10,7 +13,45 @@ var BaseModel = require('./lib/model'),
 
 
 var DataSchema = BaseModel.extend({
-	
+  distro: {
+    type : String,
+    require : true
+  },
+	source: {
+    type : String
+  }, 
+  version: {
+    type : String
+  },
+  upstream_version : {
+    type : String
+  },
+  branch : {
+    type : String
+  },
+  urgency : {
+    type : String
+  },
+  date : {
+    type : Date
+  },
+  previous_source : {
+    type : String
+  },
+  previous_version: {
+    type : String
+  },
+  cve_list : [{
+    cve : {
+      type : String
+    }
+  }], 
+  maintainer : {
+    type : String
+  },
+  comments : {
+    type : String
+  }
 });
 
 var Data = mongoose.model('Data', DataSchema);
